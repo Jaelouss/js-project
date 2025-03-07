46b0449bd1dbf680bb8c6c241e93e578d1db396a
buhaiov-v <bugayov.v.s@gmail.com>
2025-03-07 17:30:31 +0200

    start

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -2,6 +2,15 @@ import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
 import Swiper from 'swiper';
+import 'swiper/css/navigation';
 import 'swiper/css';
 
 import axios from 'axios';
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
