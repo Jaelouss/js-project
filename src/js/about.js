ada08ee37f35f8d0d255f66787bddf941c1bf260
Eugen Alekseev <devilsbrother1@gmail.com>
2025-03-10 21:26:34 +0200

    fix about me. problem with swiper

--- src/js/about.js
+++ src/js/about.js
@@ -2,39 +2,43 @@ import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation, Keyboard } from 'swiper/modules';
 import 'swiper/css/navigation';
 import 'swiper/css';
 
 import axios from 'axios';
 
-document.addEventListener("DOMContentLoaded", function () {
-    const acc=new Accordion('.accordion-container', {
-        firstOpen: true,
-        duration: 500,
-        showMultiple: false,
-        collapse: true,
-        });
-    acc.open(0);
+document.addEventListener('DOMContentLoaded', function () {
+	const acc = new Accordion('.about-accordion-container', {
+		firstOpen: true,
+		duration: 500,
+		showMultiple: false,
+		collapse: true,
+	});
+	acc.open(0);
+
+	var swiper = new Swiper('.swiper', {
+		modules: [Navigation, Keyboard],
+		slidesPerView: 'auto',
+		spaceBetween: 0,
+		centeredSlides: false,
+		loop: true,
+		navigation: {
+			nextEl: '.swiper-button-next',
+		},
+		keyboard: {
+			enabled: true,
+			onlyInViewport: true,
+		},
+
+		breakpoints: {
+			768: {
+				slidesPerView: 3,
+			},
+			1440: {
+				slidesPerView: 6,
+				allowTouchMove: false,
+			},
+		},
+	});
 });
-
-
-
-const swiper = new Swiper('#about-swiper', {
-    loop: true,
-    modules: [Navigation, Pagination],
-    slidesPerView: 2,
-    slidesPerGroup: 1,
-    spaceBetween: 1,    
-    freeMode: true,
-    centeredSlides: false,
-    speed: 500,
-    navigation: {
-        nextEl: '.swiper-button-next',
-    },
-    keyboard: {
-        enabled: true,
-        onlyInViewport: false,
-    }, 
-});
-
