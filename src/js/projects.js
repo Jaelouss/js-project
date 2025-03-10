632ec84094f21bd8244ccd12c47d9f00a8f22a3d
o_ninchuk <sashanincuk@gmail.com>
2025-03-10 22:13:33 +0200

    to reviews

--- src/js/projects.js
+++ src/js/projects.js
@@ -3,5 +3,40 @@ import 'accordion-js/dist/accordion.min.css';
 
 import Swiper from 'swiper';
 import 'swiper/css';
+import 'swiper/css/navigation';
 
-import axios from 'axios';
+import { Navigation, Keyboard } from 'swiper/modules';
+
+document.addEventListener('DOMContentLoaded', () => {
+	const swiper = new Swiper('.swiper', {
+		modules: [Navigation, Keyboard],
+		loop: false,
+		slidesPerView: 1,
+		spaceBetween: 10,
+		navigation: {
+			nextEl: '.slider-next',
+			prevEl: '.slider-prev',
+		},
+		keyboard: {
+			enabled: true,
+			onlyInViewport: false,
+		},
+		on: {
+			init: function () {
+				updateButtons(this);
+			},
+			slideChange: function () {
+				updateButtons(this);
+			},
+		},
+	});
+
+	function updateButtons(swiper) {
+		document
+			.querySelector('.slider-prev')
+			.classList.toggle('disabled', swiper.isBeginning);
+		document
+			.querySelector('.slider-next')
+			.classList.toggle('disabled', swiper.isEnd);
+	}
+});
