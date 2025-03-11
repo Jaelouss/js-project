dac91ba7fb866ce4c1de745a3abbe15f314c8e11
Eugen Alekseev <devilsbrother1@gmail.com>
2025-03-11 12:01:29 +0200

    fix render in reviews

--- src/js/reviews.js
+++ src/js/reviews.js
@@ -4,57 +4,56 @@ import 'swiper/css/navigation';
 import 'swiper/css';
 import axios from 'axios';
 
-
 const swiper = new Swiper('.reviews-swiper', {
 	modules: [Navigation, Pagination, Keyboard],
 	slidesPerView: 1,
 	spaceBetween: 16,
 	breakpoints: {
-		768: {slidesPerView: 2,},
-		1440: {slidesPerView: 4},
+		768: { slidesPerView: 2 },
+		1440: { slidesPerView: 4 },
 	},
 	navigation: {
 		nextEl: '[data-btn="next"]',
 		prevEl: '[data-btn="prev"]',
 	},
 	keyboard: {
-        enabled: true,
-        onlyInViewport: true,
-    }
+		enabled: true,
+		onlyInViewport: true,
+	},
 });
 
-
 async function fetchReviews() {
-    try {
-        const response = await axios.get('https://portfolio-js.b.goit.study/api/reviews'); 
-        return response.data;
-    } catch (error) {
-        console.error('❌ Not found', error);
-        return [];
-    }
+	try {
+		const response = await axios.get(
+			'https://portfolio-js.b.goit.study/api/reviews'
+		);
+		return response.data;
+	} catch (error) {
+		console.error('❌ Not found', error);
+		return [];
+	}
 }
 
-
 async function renderReviews() {
-    const reviews = await fetchReviews();
-    const reviewsContainer = document.querySelector('.swiper-wrapper');
+	const reviews = await fetchReviews();
+	const reviewsContainer = document.querySelector('[data-swiper="reviews"]');
 
-    if (!reviewsContainer) return;
+	if (!reviewsContainer) return;
 
-    reviewsContainer.innerHTML = '';
+	reviewsContainer.innerHTML = '';
 
-    reviews.forEach(review => {
-        const reviewItem = `
+	reviews.forEach(review => {
+		const reviewItem = `
             <li class="swiper-slide swiper-id">
                 <img class="swiper-avatar_url" src="${review.avatar_url}" width="48" height="48" alt="${review.author}">
                 <h3 class="swiper-author">${review.author}</h3>
                 <p class="swiper-review">${review.review}</p>
             </li>
         `;
-        reviewsContainer.insertAdjacentHTML('beforeend', reviewItem);
-    });
+		reviewsContainer.insertAdjacentHTML('beforeend', reviewItem);
+	});
 
-    swiper.update();
+	swiper.update();
 }
 
 document.addEventListener('DOMContentLoaded', renderReviews);
